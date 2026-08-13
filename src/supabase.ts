e99bb1f6081e9abd172da3459941e924f853fd03
prrodrigoduarte-sys <prrodrigoduarte@gmail.com>
2026-08-13 15:18:35 -0300

    alt

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = 'https://knddosboqjeaefqxdmmt.supabase.co';
+const supabaseAnonKey = 'sb_publishable_kHGViXsw4za38lhDwJPbwQ_CBWn92dK';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
